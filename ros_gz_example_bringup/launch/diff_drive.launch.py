4c9d5cec1fa33fbcc97f6866a0038e3e7fed6dd8
hulehe <huzhenkai@naver.com>
2025-06-10 16:51:26 +0900

    model.urdf used

--- ros_gz_example_bringup/launch/diff_drive.launch.py
+++ ros_gz_example_bringup/launch/diff_drive.launch.py
@@ -36,8 +36,8 @@ def generate_launch_description():
     pkg_ros_gz_sim = get_package_share_directory('ros_gz_sim')
 
     # Load the SDF file from "description" package
-    sdf_file  =  os.path.join(pkg_project_description, 'models', 'diff_drive', 'model.sdf')
-    with open(sdf_file, 'r') as infp:
+    urdf_file  =  os.path.join(pkg_project_description, 'models', 'diff_drive', 'model.urdf')
+    with open(urdf_file, 'r') as infp:
         robot_desc = infp.read()
 
     # Setup to launch the simulator and Gazebo world
