7e21839a8adc1fed0595939da883a3b31cd0ce27
hulehe <huzhenkai@naver.com>
2025-06-17 21:08:00 +0900

    wall.xacro added

--- ros_gz_bringup/launch/sensor_world.launch.py
+++ ros_gz_bringup/launch/sensor_world.launch.py
@@ -49,9 +49,16 @@ def launch_setup(context, *args, **kwargs):
     ).toxml()
     
 
-    wall_sdf_file  =  os.path.join(pkg_gz_models, 'models', 'sdf', 'wall', 'model.sdf')
-    with open(wall_sdf_file, 'r') as infp:
-        wall_desc = infp.read()
+    # wall_sdf_file  =  os.path.join(pkg_gz_models, 'models', 'sdf', 'wall', 'model.sdf')
+    # with open(wall_sdf_file, 'r') as infp:
+    #     wall_desc = infp.read()
+    wall_xacro_file  =  os.path.join(pkg_gz_models, 'models', 'urdf', 'wall.xacro')
+    wall_desc = xacro.process_file(
+        wall_xacro_file,
+        mappings={
+            'robot_model_name': robot_model_name  # 设置外部参数
+        }
+    ).toxml()
 
     # Setup to launch the simulator and Gazebo world
     world_file = os.path.join(pkg_gz_worlds,'worlds','sensor_world.sdf')
@@ -71,6 +78,18 @@ def launch_setup(context, *args, **kwargs):
             ('robot_description', robot_model_name + '_description'),
             ('joint_states', robot_model_name + '/joint_states')
         ]
+    )
+
+    wall_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='wall_state_publisher',
+        output='screen',
+        parameters=[
+            {'use_sim_time': True},
+            {'robot_description': wall_desc},
+        ],
+        remappings=[('robot_description', 'wall_description')]
     )
 
     bridge = Node(
@@ -99,6 +118,16 @@ def launch_setup(context, *args, **kwargs):
         output='screen'
     )
 
+    wall = Node(
+        package='ros_gz_sim',
+        executable='create',
+        arguments=['-topic', '/wall_description', 
+                   '-name', 'wall',
+                   '-x', '5'
+                   ],
+        output='screen'
+    )
+
     # Visualize in RViz
     rviz = Node(
        package='rviz2',
@@ -108,9 +137,11 @@ def launch_setup(context, *args, **kwargs):
     )
 
     return [
-        robot_state_publisher,         
+        robot_state_publisher,  
+        wall_state_publisher,       
         gz_sim, 
         diff_drive, 
+        wall,
         bridge, 
         rviz
         ]
